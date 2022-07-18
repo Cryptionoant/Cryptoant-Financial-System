ee4daf52d060a42ce7bc06dda7ef6f13ce2d73cf
Cryptoants <94941606+Cryptionoant@users.noreply.github.com>
2022-07-18 13:53:17 +0800

    Update Cry.sol

--- contracts/token/Cry.sol
+++ contracts/token/Cry.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Cry is ERC20{
-    
+
     address public admin;
     event AdminChange(address indexed Admin, address indexed newAdmin);
     constructor(address manager) public ERC20("Cry", "Cry") {
