3722718377d13ca011580bc91f10b8e97bf0276b
Cryptoants <94941606+Cryptionoant@users.noreply.github.com>
2022-08-03 09:14:34 +0800

    Update Cryptionoant.sol

--- contracts/Cryptionoant/Cryptionoant.sol
+++ contracts/Cryptionoant/Cryptionoant.sol
@@ -66,6 +66,7 @@ contract Cryptoant{
         return _tokenApprovals[tokenId];
     }
     
+    
      function _exists(uint256 tokenId) public view  returns (bool) {
         return list[tokenId].owner != address(0);
     }
